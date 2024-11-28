92822eaf879783361399fd940f5c4206505bd7e6
Heshani <heshaninarmada57@gmail.com>
2024-11-28 00:22:59 -0800

    updated product model

--- model/product.js
+++ model/product.js
@@ -1,12 +1,18 @@
 import mongoose from "mongoose";
 
 const productSchema = mongoose.Schema({
-    name :String,
-    price :Number,
-    description: String,
+    name:{ type:String, require:true},
+    description:{type:String,require:true },
+    price:{type:Number, require:true},
+    image:{type:String,require:true},
+    category:{type:Array},
+    wholesaleprice:{type:Number},
+    wholesaleminmumQuntity:{type:Number},
+    concern:{type:Array},
+    inStock: {type:Boolean, default:true }
 })
 
 const Product = mongoose.model("Product",
     productSchema)
 
-export default Product
+export default Product;
